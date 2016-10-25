7af5da73827edc0dafbfd15dbfab5a0d9bbb9fe3
Holman Gao <holman@golmansax.com>
2016-10-24 21:59:22 -0400

    Add accept request button to provider

--- app/components/scenes/nalo_provider/home_scene.js
+++ app/components/scenes/nalo_provider/home_scene.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { Component, PropTypes } from 'react';
 import { StyleSheet } from 'react-native';
-import { Text } from '../../base';
+import { Actions } from 'react-native-router-flux';
+import { View, Text, Button } from '../../base';
 import { firebaseDB } from '../../../lib/firebase';
+import { RequestStatus } from '../../../lib/constants';
 
 const styles = StyleSheet.create({
   content: {
@@ -10,6 +12,47 @@ const styles = StyleSheet.create({
   },
 });
 
-export const NaloProviderHomeScene = () => (
-  <Text style={styles.content}>Provider</Text>
-);
+export class NaloProviderHomeScene extends Component {
+  static propTypes = {
+    request: PropTypes.object.isRequired,
+  };
+
+  static defaultProps = {
+    request: {},
+  };
+
+  componentWillMount() {
+    this.listener = firebaseDB().ref('request').on('value', (snapshot) => {
+      const newRequest = snapshot.val();
+      if (newRequest.status !== this.props.request.status) {
+        Actions.refresh({ request: newRequest });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    firebaseDB().database().off(this.listener);
+  }
+
+  render() {
+    const { request } = this.props;
+    const { status, source } = request;
+
+    return (
+      <View style={styles.content}>
+        <Text>Provider</Text>
+        {status === RequestStatus.REQUESTED ? (
+          <View>
+            <Text>REQUEST</Text>
+            <Text>{source.title}</Text>
+            <Button onPress={this.acceptRequest}>Deliver Naloxone</Button>
+          </View>
+        ) : null}
+      </View>
+    );
+  }
+
+  acceptRequest = () => {
+    firebaseDB().ref('request/status').set(RequestStatus.ACCEPTED);
+  };
+}
