aa8c5053d1f0ff1b0d543202f27b85315d265e31
Holman Gao <holman@golmansax.com>
2016-10-25 08:32:53 -0400

    Throw an error for maps url if on android

--- app/components/scenes/nalo_provider/accepted_request_scene.js
+++ app/components/scenes/nalo_provider/accepted_request_scene.js
@@ -1,17 +1,26 @@
 import React, { Component } from 'react';
+import { Platform, Linking } from 'react-native';
 import URI from 'urijs';
-import { Linking } from 'react-native';
 import { View } from '../../base';
 import { providerLocation, responderLocation } from '../../../lib/data';
 import { locationToString } from '../../../lib/utils';
 
 async function openDirections() {
-  const url = new URI('https://maps.apple.com')
-    .search({
-      saddr: locationToString(providerLocation),
-      daddr: locationToString(responderLocation),
-    })
-    .toString();
+  const url = (() => {
+    const { OS } = Platform;
+
+    switch (OS) {
+      case 'ios':
+        return new URI('https://maps.apple.com')
+          .search({
+            saddr: locationToString(providerLocation),
+            daddr: locationToString(responderLocation),
+          })
+          .toString();
+
+      default: throw new Error(`Directions url not specified for OS: ${OS}`);
+    }
+  })();
 
   if (!Linking.canOpenURL(url)) {
     throw new Error(`Cannot link to URL: ${url}`);
